7ee174e4c4d2ea04f46fc7321d86e34d82c96f1c
RKana <rkanamail@gmail.com>
2017-06-15 23:45:32 -0400

    creamsight initial

---
+++ start.sh
@@ -0,0 +1,9 @@
+export INSIGHT_NETWORK=livenet
+export BITCOIND_USER=Ulysseys
+export BITCOIND_PASS=H9fsJ6RYQASypXBHAXjSDBCdHAhH6USQp3gd7m1tR7D
+export BITCOIND_P2P_PORT=15714
+export BITCOIND_DATADIR=/home/rkanadev/.Cream
+export BITCOIND_PORT=8332 
+export NODE_ENV=production
+export INSIGHT_PORT=3000
+npm start
